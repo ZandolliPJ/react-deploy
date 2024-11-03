f4637debb16aa7c98297f76ffaf8706818b6030c
ZandolliPJ <judithpascal07@gmail.com>
2024-11-03 21:34:41 +0100

    Image animé

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import catGif from "./cat.gif";
 
 function App() {
-  return <div className="App">test</div>;
+  return (
+    <div className="App">
+      <img src={catGif} alt="cat Gif" />
+      <h1>
+        Le lorem ipsum (également appelé faux-texte, lipsum, ou bolo bolo) est,
+        en imprimerie, une suite de mots sans signification utilisée à titre
+        provisoire pour
+      </h1>
+    </div>
+  );
 }
 
 export default App;
